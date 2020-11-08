30d62b7b79c9e49fc3141f11db5591e92875ba9a
lupus-rex <starkdeals@yahoo.com>
2020-11-08 12:46:38 -0500

    integrated firebase into application, created sign in page with sign in component and sign in with google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyCGEiKpcMKLCLikWw-1w941_aC1V7aefZA",
+  authDomain: "alphacloning.firebaseapp.com",
+  databaseURL: "https://alphacloning.firebaseio.com",
+  projectId: "alphacloning",
+  storageBucket: "alphacloning.appspot.com",
+  messagingSenderId: "254040229962",
+  appId: "1:254040229962:web:5c4c4c92e9bc438a493eb3",
+  measurementId: "G-S4RM1DZ9G5"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
