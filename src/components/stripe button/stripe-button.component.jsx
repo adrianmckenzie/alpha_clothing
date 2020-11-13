21b630cca85c82be1d74a6d2981d72cdeb09acf8
lupus-rex <starkdeals@yahoo.com>
2020-11-12 19:56:40 -0500

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51GxKFoDyHwcA7oQtK8cPbH8mBPdrLyDVa4ZYeQEnpLUBU1RmXv5N2kWrJvJmdKnW8kXrkdXHaKR8Z0UGcwubIp3l00UQyr50N8";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Alpha Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
